7aa00f916999a83f3ac95573b4c7ff50d6b82581
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-07-06 16:18:03 -0700

    init: selector-excercise.js

---
+++ 15-DOM-Manipulation/selector-excercise.js
@@ -0,0 +1,4 @@
+var tag = document.getElementById("first");
+var tag2 = document.getElementsByClassName("special");
+var tag3 = document.querySelector("p");
+var tag4 = document.querySelector("#first");
