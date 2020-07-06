42f6308c641b40106b35354fc2edd2d0d4c43487
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-07-06 16:42:35 -0700

    feat: style manipulating examples

---
+++ 15-DOM-Manipulation/manipulating.js
@@ -0,0 +1,9 @@
+// Generic example of manipulating style
+var h1 = document.querySelector("h1");
+h1.style.color = "Red";
+h1.style.border = "5px solid black";
+
+// Adding a class to an tag
+var p = document.querySelector("p");
+p.classList; // EMPTY
+p.classList.add(".big");
