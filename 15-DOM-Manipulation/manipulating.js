989e6a85492bd9738f654faf1be49e5f03ee59cd
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-07-06 20:08:41 -0700

    feat: manipulating attributes

--- 15-DOM-Manipulation/manipulating.js
+++ 15-DOM-Manipulation/manipulating.js
@@ -15,5 +15,12 @@ var p1 = document.getElementsByTagName("p")[0];
 console.log(p1.textContent);
 var ul = document.querySelector("ul");
 ul.innerHTML = "<strong>Hello there!</strong>";
-
 var test = document.querySelector("h1").textContent = "GENERAL KENOBI!";
+
+// Manipulating attributes
+var img1 = document.getElementsByTagName("img")[0];
+img1.setAttribute("src", "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Foperationrainfall.com%2Fwp-content%2Fuploads%2F2014%2F03%2FMaster-Chief.jpg&f=1&nofb=1");
+
+var a = document.querySelector("a");
+a.setAttribute("href", "https://github.com/Brian-E-Nguyen/The-Web-Developer-Bootcamp/tree/15-DOM-manipulation/15-DOM-Manipulation")
+a.textContent = "Link to this repository on my GitHub!"
