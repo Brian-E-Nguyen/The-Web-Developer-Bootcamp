decd5575a4e5993ed608403be80d85d175489926
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-07-07 12:50:57 -0700

    fix: improved algorithm

--- 16-Advanced-DOM-Manipulation/todo.js
+++ 16-Advanced-DOM-Manipulation/todo.js
@@ -2,10 +2,10 @@ var lis = document.querySelectorAll("li");
 
 for (let i = 0; i < lis.length; i++) {
     lis[i].addEventListener("mouseover", function(){
-        this.style.color = "Green";
+        this.classList.add("selected");
     })
     lis[i].addEventListener("mouseout", function(){
-        this.style.color = "Black";
+        this.classList.remove("selected");
     })
     lis[i].addEventListener("click", function(){
         this.classList.toggle("done")
