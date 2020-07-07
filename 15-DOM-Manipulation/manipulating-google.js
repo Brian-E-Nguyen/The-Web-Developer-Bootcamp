48287a1d004f653d047f88b9de1a0c9d1d0c8c74
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-07-06 22:08:52 -0700

    feat: changing Google logo

---
+++ 15-DOM-Manipulation/manipulating-google.js
@@ -0,0 +1,4 @@
+var logo = document.querySelector("#hplogo")
+logo.setAttribute("srcset", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F6%2F6a%2FPomeranian_600.jpg&f=1&nofb=1");
+logo.style.width = "200px"
+logo.style.height = "100px"
