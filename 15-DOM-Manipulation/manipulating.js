46d7ac84f8969af73cd8a825e84fc39abe5f911e
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-07-06 18:59:46 -0700

    -mfeat: adding classes

--- 15-DOM-Manipulation/manipulating.js
+++ 15-DOM-Manipulation/manipulating.js
@@ -3,7 +3,9 @@ var h1 = document.querySelector("h1");
 h1.style.color = "Red";
 h1.style.border = "5px solid black";
 
-// Adding a class to an tag
+// Adding and removing a class to an tag
 var p = document.querySelector("p");
 p.classList; // EMPTY
 p.classList.add(".big");
+p.classList.remove(".big");
+p.classList.toggle(".big");
