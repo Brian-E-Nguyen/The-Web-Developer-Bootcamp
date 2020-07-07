c4d40cdea2a3f3c6540f84e46e97609796594ea6
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-07-07 12:18:44 -0700

    feat: event listener to reset button

--- 16-Advanced-DOM-Manipulation/scorekeeper.js
+++ 16-Advanced-DOM-Manipulation/scorekeeper.js
@@ -1,7 +1,8 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
-var p1Display = document.querySelector("#p1Display")
-var p2Display = document.querySelector("#p2Display")
+var resetButton = document.getElementById("reset");
+var p1Display = document.querySelector("#p1Display");
+var p2Display = document.querySelector("#p2Display");
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
@@ -30,4 +31,14 @@ p2Button.addEventListener("click", function(){
         }
         p2Display.textContent = p2Score;
     }
+})
+
+resetButton.addEventListener("click", function(){
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove("winner")
+    p2Display.classList.remove("winner")
+    gameOver = false;
 })
