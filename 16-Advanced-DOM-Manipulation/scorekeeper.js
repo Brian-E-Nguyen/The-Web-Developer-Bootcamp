e8b072cee50f95b76eb013cd1ed6535763c10013
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-07-07 12:33:46 -0700

    fix: bugs when reseting scores

--- 16-Advanced-DOM-Manipulation/scorekeeper.js
+++ 16-Advanced-DOM-Manipulation/scorekeeper.js
@@ -3,6 +3,9 @@ var p2Button = document.getElementById("p2");
 var resetButton = document.getElementById("reset");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
+var numInput = document.querySelector("input[type='number'")
+var p = document.querySelector("p");
+var winningScoreDisplay = document.querySelector("p span")
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
@@ -18,22 +21,31 @@ p1Button.addEventListener("click", function(){
         }
         p1Display.textContent = p1Score;
     }
-    
 })
 
 p2Button.addEventListener("click", function(){
     if(!gameOver){
         p2Score++;
         if(p2Score === winningScore){
-            console.log("Game over!")
+            console.log("Game over!");
             gameOver = true;
-            p2Display.classList.add("winner")
+            p2Display.classList.add("winner");
         }
         p2Display.textContent = p2Score;
     }
 })
 
 resetButton.addEventListener("click", function(){
+    reset();
+})
+
+numInput.addEventListener("change", function(){
+    winningScoreDisplay.textContent = numInput.value;
+    winningScore = Number(numInput.value);
+    reset();
+})
+
+function reset(){
     p1Score = 0;
     p2Score = 0;
     p1Display.textContent = 0;
@@ -41,4 +53,4 @@ resetButton.addEventListener("click", function(){
     p1Display.classList.remove("winner")
     p2Display.classList.remove("winner")
     gameOver = false;
-})
+}
