682f18d0ece301d4aa6f7f16675a485c89cf674b
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-07-08 12:53:00 -0700

    feat: styling entire website

--- 17-Color-Game-Project/color-game.js
+++ 17-Color-Game-Project/color-game.js
@@ -51,7 +51,7 @@ resetButton.addEventListener("click", function() {
         squares[i].style.backgroundColor = colors[i];
         
     }
-    h1.style.background = "#232323"
+    h1.style.background = "steelblue"
 }) 
 
 colorDisplay.textContent = pickedColor;
