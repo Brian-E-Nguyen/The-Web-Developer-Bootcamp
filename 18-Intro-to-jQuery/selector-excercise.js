e5926e18bdcddb73f5d13221c557089361e94dd5
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-07-09 12:15:14 -0700

    feat: solutions to excercise

---
+++ 18-Intro-to-jQuery/selector-excercise.js
@@ -0,0 +1,11 @@
+// Select all divs and give them a purple background
+$("div").css("backgroundColor", "purple");
+
+// Select all the divs with class "highlight" and make them 200px wide
+$(".highlight").css("border", "200px solid");
+
+// Select the div with id "third" and give it an orange border
+$("#third").css("border", "2px solid orange");
+
+// Bonus: Select the first div only and change its font color to pink
+$("div:first").css("color", "pink");
