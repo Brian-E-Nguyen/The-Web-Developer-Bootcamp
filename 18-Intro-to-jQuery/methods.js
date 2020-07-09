1da86d5476739e6f5f78cd710ea8dbe2c543a29c
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-07-09 14:57:59 -0700

    feat: code for testing selecting

---
+++ 18-Intro-to-jQuery/methods.js
@@ -0,0 +1,10 @@
+// text();
+var h1Text = $("h1").text();
+var lis = $("li").text(); // SkittlesStarburstTwix
+
+$("h1").text("New text!");
+$("li").text("Mimi and Kiki, Brian's dogs, are adorable");
+
+// .html();
+var ul = $("ul").html();
+$('ul').html("<li>I hacked your UL!</li><li>Mimi is the best</li>")
