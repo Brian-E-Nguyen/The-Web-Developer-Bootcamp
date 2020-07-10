ad88696d093a6b6491da27c35b379ebba260f537
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-07-10 12:51:53 -0700

    feat: tests with fading and sliding

---
+++ 19-Advanced-jQuery/effects.js
@@ -0,0 +1,11 @@
+// $('button').on('click', function() {
+//     $('div').fadeOut(5000, function() {
+//         console.log("Fade completed!");
+//         // Removes each div element
+//         $(this).remove();
+//     });
+// });
+
+$('button').on('click', function() {
+    $('div').slideDown(5000);
+})
