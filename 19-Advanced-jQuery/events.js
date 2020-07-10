394b4dbbd41c3f74a9a0a691e5a461f57b23b6da
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-07-10 12:21:16 -0700

    feat: testing on()

--- 19-Advanced-jQuery/events.js
+++ 19-Advanced-jQuery/events.js
@@ -1,16 +1,33 @@
-// click()
-$('h1').click(function() {
-    alert('h1 clicked')
+// // click()
+// $('h1').click(function() {
+//     alert('h1 clicked')
+// });
+
+// $('button').click(function() {
+//     $(this).css('background', 'pink')
+// });
+
+// // keypress()
+// $('input').keypress(function(event) {
+//     // If the user hits enter
+//     if(event.which === 13) {
+//         alert('you hit enter!');
+//     }
+// });
+
+// on()
+$('h1').on('click', function() {
+    $(this).css('color', 'purple')
 });
 
-$('button').click(function() {
-    $(this).css('background', 'pink')
+$('input').on('keypress', function() {
+    console.log('keypressed!')
+})
+
+$('button').on('mouseenter', function() {
+    $(this).css('font-weight', 'bold')
 });
 
-// keypress()
-$('input').keypress(function(event) {
-    // If the user hits enter
-    if(event.which === 13) {
-        alert('you hit enter!');
-    }
+$('button').on('mouseleave', function() {
+    $(this).css('font-weight', 'normal')
 });
