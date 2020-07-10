87d5c0a7624cccadf9c62447e0afc3d25ac3b316
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-07-10 12:03:58 -0700

    feat: tests with keypress()

--- 19-Advanced-jQuery/events.js
+++ 19-Advanced-jQuery/events.js
@@ -1,7 +1,16 @@
+// click()
 $('h1').click(function() {
     alert('h1 clicked')
 });
 
 $('button').click(function() {
     $(this).css('background', 'pink')
+});
+
+// keypress()
+$('input').keypress(function(event) {
+    // If the user hits enter
+    if(event.which === 13) {
+        alert('you hit enter!');
+    }
 });
