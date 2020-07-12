c376125e3940a3c71657a6b848ac18d20ddf99f2
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-07-12 11:47:19 -0700

    feat: delete todo

--- 20-Todo-List-Project/assets/js/todos.js
+++ 20-Todo-List-Project/assets/js/todos.js
@@ -1,4 +1,15 @@
 // Check off specific to-do's by clicking
 $("li").click(function() {
     $(this).toggleClass("completed");
-})
+});
+
+// Click on X to delete to-do. This will cause event bubbling
+$("span").click(function(event) {
+    // Gives us the parent of the current element (li)
+    $(this).parent().fadeOut(500, function() {
+        // Refers to the li, not the span
+        $(this).remove();
+    });
+    // Prevents event bubbling
+    event.stopPropagation();
+});
