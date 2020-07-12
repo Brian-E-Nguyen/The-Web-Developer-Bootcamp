c001a15c38e05212ccf448711d88f76059ac1aa2
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-07-12 11:56:19 -0700

    feat: add todo; refactoring

--- 20-Todo-List-Project/assets/js/todos.js
+++ 20-Todo-List-Project/assets/js/todos.js
@@ -1,10 +1,11 @@
 // Check off specific to-do's by clicking
-$("li").click(function() {
+// When an li is clicked within the ul, run this code
+$("ul").on("click", "li", function() {
     $(this).toggleClass("completed");
 });
 
 // Click on X to delete to-do. This will cause event bubbling
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
     // Gives us the parent of the current element (li)
     $(this).parent().fadeOut(500, function() {
         // Refers to the li, not the span
@@ -12,4 +13,15 @@ $("span").click(function(event) {
     });
     // Prevents event bubbling
     event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+    if(event.which === 13) {
+        // Grab todo text
+        var todoText = $(this).val();
+        // Clears input
+        $(this).val("");
+        // Create a new li and add to ul
+        $("ul").append(`<li><span>X</span> ${todoText}</li>`)
+    }
 });
