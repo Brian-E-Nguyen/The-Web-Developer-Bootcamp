066bf69aba85ee14b9d2ea41aee89631b8438b26
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-07-16 10:52:24 -0700

    feat: init excercise

---
+++ 24-Intro-To-Node/excercise.js
@@ -0,0 +1,10 @@
+// NODE EXCERCISE
+
+// Using the commandline, create a file "echo.js" 
+// Inside the file, write a function named "echo()" that takes two arguments: a string and a number
+// It should print out the string x number of times
+
+echo('Echo!!!', 10);
+echo('Tater tots', 3);
+
+// Run the contents of 'echo.js' using Node
