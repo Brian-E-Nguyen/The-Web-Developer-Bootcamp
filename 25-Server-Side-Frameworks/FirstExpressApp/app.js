3c8bf657417c580ec4bae20e6e0e0297b9b5ea9e
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-07-17 11:40:48 -0700

    feat: testing route params

--- 25-Server-Side-Frameworks/FirstExpressApp/app.js
+++ 25-Server-Side-Frameworks/FirstExpressApp/app.js
@@ -19,6 +19,24 @@ app.get('/dog', function(req, res) {
     res.send('Meow');
 });
 
+app.get('/r/:subredditName', function(req, res) {
+    // console.log(req.params);
+    var subreddit = req.params.subredditName;
+    res.send(`WELCOME TO THE ${subreddit.toUpperCase()} SUBREDDIT!`);
+});
+
+app.get('/r/:subredditName/comments/:id/:title/', function(req, res) {
+    console.log(req.params);
+    res.send('Welcome to the comments page!');
+});
+
+// Directs to this URL from any request other than the 3 above
+// THE ORDER OF THE ROUTES MATTER
+// When a request is made, it checks all routes going down the list
+app.get('*', function(req, res) {
+    res.send('You are a star!')
+})
+
 // Tell Express to listen for requests (start server)
 app.listen(3000, function() {
     console.log('Server has started');
