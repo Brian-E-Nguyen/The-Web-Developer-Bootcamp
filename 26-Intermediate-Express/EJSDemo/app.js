876e0d8ac8b481d2312e169825c20fba40899e43
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-07-18 11:18:35 -0700

    feat: testing control flow

--- 26-Intermediate-Express/EJSDemo/app.js
+++ 26-Intermediate-Express/EJSDemo/app.js
@@ -6,6 +6,16 @@ app.get('/', function(req, res) {
     res.render('home.ejs');
 });
 
+app.get('/posts', function(req, res) {
+    var posts = [
+        {title: 'Post 1', author: 'Susy'},
+        {title: 'Adorable Mimi', author: 'Kiki'},
+        {title: 'Poms Are the Best', author: 'Brian'},
+    ];
+
+    res.render('posts.ejs', {posts: posts})
+})
+
 app.get('/fallinlovewith/:thing', function(req, res) {
     var thing = req.params.thing;
     res.render('love.ejs', {thingVar: thing});
