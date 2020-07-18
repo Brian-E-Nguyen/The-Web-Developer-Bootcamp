e559d0066490e393faae654483c9ffb56850a0e9
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-07-18 10:58:50 -0700

    feat: testing ejs

--- 26-Intermediate-Express/EJSDemo/app.js
+++ 26-Intermediate-Express/EJSDemo/app.js
@@ -1,2 +1,16 @@
 var express = require('express');
 var app = express();
+const portNumber = 1193;
+
+app.get('/', function(req, res) {
+    res.render('home.ejs');
+});
+
+app.get('/fallinlovewith/:thing', function(req, res) {
+    var thing = req.params.thing;
+    res.render('love.ejs', {thingVar: thing});
+});
+
+app.listen(portNumber, function() {
+    console.log(`Server started at port ${portNumber}`)
+});
